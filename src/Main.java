736d7bce207818cadb05415b972991745fab5a8e
betterremm <doabledguy@gmail.com>
2025-03-06 16:38:10 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,13 @@ public class Main {
     public static final Scanner scan = new Scanner(System.in);
     public static final int MIN_CHOICE = 0;
     public static final int MAX_CHOICE = 6;
+    public static final int MAX_TRAIN_CHOICE = 4;
     public static final int MIN_TRAINNUM = 0;
-    public static final int MAX_TRAINNUM = 10000;
+    public static final int MAX_TRAINNUM = 1000000;
+    public static final int MIN_HOUR = 0;
+    public static final int MAX_HOUR = 23;
+    public static final int MIN_MINUTE = 0;
+    public static final int MAX_MINUTE = 59;
     public enum Status
     {
         GOOD,
@@ -21,7 +26,11 @@ public class Main {
         BAD_FILE,
         NOT_AN_INT,
         OUT_OF_RANGE,
-        WRONG_SERIALIZATION
+        WRONG_SERIALIZATION,
+        OUT_OF_RANGE_STR,
+        OUT_OF_RANGE_TIME,
+        OUT_OF_RANGE_TICKETS,
+        BAD_FILE_NAME
     }
 
 
@@ -30,11 +39,15 @@ public class Main {
             "Этот файл пустой.\n",
             "Данный файл невозможно прочитать.\n",
             "Недостаточно прав для записи в этот файл.\n",
-            "Введенное число не соответствует опциям выбора.\n",
+            "Введенные данные не соответствуют опциям выбора.\n",
             "Файл содержит некорректные данные.\n",
             "Введенное число не является целым числом в заданном диапазоне.\n",
             "Введенное значение вне диапазона.\n",
-            "Данный файл .dat был сохранен другой программой.\n"
+            "Данный файл .dat был сохранен другой программой.\n",
+            "Строка слишком большого размера.\n",
+            "Время задано неправильно.\n",
+            "Неправильный ввод. Попробуйте еще раз.\n",
+            "Название файла должно быть на английском.\n"
     };
 
 
@@ -100,6 +113,9 @@ public class Main {
             else {
                 err = Status.NOT_DAT;
             }
+            if(!pathToFile.matches("^[a-zA-Z0-9._-]+$")){
+                err = Status.BAD_FILE_NAME;
+            }
             System.err.print(ERRTEXT[err.ordinal()]);
         }while(err != Status.GOOD);
         return  pathToFile;
@@ -127,41 +143,41 @@ public class Main {
     }
 
 
-    public static String createTrainsDat(List<Train> trains, String currentPath){
-        Train.saveTrains(trains, currentPath);
-        currentPath = getFilePath();
-        File f = new File(currentPath);
+    public static String createTrainsDat(List<Train> trains, String[] currentPath){
+        Train.saveTrains(trains, currentPath[0]);
+        currentPath[0] = getFilePath();
+        File f = new File(currentPath[0]);
         if(f.exists()){
             if(askToDelete(f) == 1) {
                 f.delete();
-                Train.saveTrains(trains, currentPath);
+                Train.saveTrains(trains, currentPath[0]);
             }
             else {
-                Train.loadTrains(trains, currentPath);
+                Train.loadTrains(trains, currentPath[0]);
             }
         }
             else {
-            Train.saveTrains(trains, currentPath);
+            Train.saveTrains(trains, currentPath[0]);
         }
 
-            return currentPath;
+            return currentPath[0];
     }
 
-    public static String openTrainsDat(List<Train> trains, String currentPath){
+    public static String openTrainsDat(List<Train> trains, String[] currentPath){
         boolean isReadable;
         do {
             isReadable = false;
-            currentPath = getFilePath();
-            File f = new File(currentPath);
+            currentPath[0] = getFilePath();
+            File f = new File(currentPath[0]);
             if (f.canRead()) {
-                Train.loadTrains(trains, currentPath);
+                Train.loadTrains(trains, currentPath[0]);
                 isReadable = true;
             } else {
                 System.out.println(ERRTEXT[3]);
             }
         }while(!isReadable);
 
-        return currentPath;
+        return currentPath[0];
     }
 
     public static int getTrainNumber(){
@@ -170,7 +186,8 @@ public class Main {
         int number = 0;
         do {
             err = Status.GOOD;
-            writeOptions();
+            System.out.println("Введите номер поезда. (1-1000000)");
+
             try {
                 number = Integer.parseInt(scan.nextLine());
             } catch (NumberFormatException e) {
@@ -206,43 +223,254 @@ public class Main {
         }
     }
 
+
+    public static void showTrain(Train train){
+        System.out.println("\tНомер поезда: " + train.trainNumber);
+        System.out.println("\tСтанция назначения: " + train.stationDest); //sta time tick
+        System.out.println("\tВремя отправления: " + train.departureHour + ':' + train.departureMinute);
+        if(train.isThereTickets){
+            System.out.println("\tБилеты на поезд есть");
+        }
+        else {
+            System.out.println("\tБилетов на поезд нет");
+        }
+    }
+
     public static void showTrainsInTime(List<Train> trains){
-        //get times (A - B)
-        //find trains that are between A and B
-        //show them
+        int[][] timeInterval = new int[2][2];
+        System.out.println("Введите начало временного интервала для поиска.");
+        timeInterval[0] = getDepartureTime();
+        System.out.println("Введите конец временного интервала для поиска.");
+        timeInterval[1] = getDepartureTime();
+        for(Train train : trains){
+            if(train.departureHour > timeInterval[0][0] && train.departureHour < timeInterval[1][0]
+            && train.departureMinute > timeInterval[0][1] && train.departureMinute < timeInterval[1][1]){
+                showTrain(train);
+            }
+        }
     }
 
     public static void showAllTrains(List<Train> trains){
-        //show trains)
+
+        for(int i = 0; i < trains.size(); i++){
+            System.out.print(i + 1 + ")");
+            showTrain(trains.get(i));
+        }
     }
 
-    public static void deleteTrain(List<Train> trains, String currentPath){
-        //get a train to delete
-        //delete a train
-        //train.savetrains
+    public static int chooseTrain(List<Train> trains){
+        showAllTrains(trains);
+        Status err;
+        int number = 0;
+        do {
+            err = Status.GOOD;
+            System.out.println("Введите порядковый номер поезда.");
+
+            try {
+                number = Integer.parseInt(scan.nextLine());
+            } catch (NumberFormatException e) {
+                err = Status.NOT_AN_INT;
+            }
+            if (err == Status.GOOD && (number < 1 || number > trains.size())) {
+                err = Status.OUT_OF_RANGE;
+            }
+            System.err.print(ERRTEXT[err.ordinal()]);
+        } while (err != Status.GOOD);
+
+        return number;
+
+
     }
 
-    public static void changeTheTrain(List<Train> trains, String currentPath){
-        //get a train to change
-        //while(boolean smth)
-        //get what to change or change nothing
-        //train.savetrains
+    public static void deleteTrain(List<Train> trains, String[] currentPath){
+        int number = chooseTrain(trains) - 1;
+        trains.remove(number);
+        Train.saveTrains(trains, currentPath[0]);
     }
 
-    public static void addTheTrain(List<Train> trains, String currentPath){
-        //get a train number
-        //stationDest
-        //departure hour and minute
-        //is there tickets
+    public static void writeTrainOptions(){
+        System.out.println("""
+                Выберите что хотите изменить
+                0 - ничего;
+                1 - номер поезда;
+                2 - станция назначения;
+                3 - время отправления;
+                4 - наличие билетов.
+                """);
+    }
 
-        //train.saveTrains
+    public static int getChoiceToChange(){
+        Status err;
+        int input = 0;
+        do {
+            err = Status.GOOD;
+            writeTrainOptions();
+            try {
+                input = Integer.parseInt(scan.nextLine());
+            } catch (NumberFormatException e) {
+                err = Status.NOT_AN_INT;
+            }
+            if (err == Status.GOOD && (input < MIN_CHOICE || input > MAX_TRAIN_CHOICE)) {
+                err = Status.OUT_OF_RANGE;
+            }
+            System.err.print(ERRTEXT[err.ordinal()]);
+        } while (err != Status.GOOD);
+
+
+        return input;
+
+    }
+
+
+    public static void changeTheTrain(List<Train> trains, String[] currentPath){
+        int choice;
+        Train temp;
+        int number = chooseTrain(trains) - 1;
+        do {
+            choice = getChoiceToChange();
+
+            switch (choice) {
+                case 1:
+                    temp = trains.get(number);
+                    temp.trainNumber = getTrainNumber();
+                    trains.set(number, temp);
+                    break;
+                case 2:
+                    temp = trains.get(number);
+                    temp.stationDest = getStation();
+                    trains.set(number, temp);
+                    break;
+                case 3:
+                    temp = trains.get(number);
+                    int[] times;
+                    times = getDepartureTime();
+                    temp.departureHour = times[0];
+                    temp.departureMinute = times[1];
+                    trains.set(number, temp);
+                    break;
+                case 4:
+                    temp = trains.get(number);
+                    temp.isThereTickets = getTickets();
+                    break;
+            }
+
+        }while(choice > 0);
+        Train.saveTrains(trains, currentPath[0]);
+    }
+
+    public static String getStation(){
+        String input = "";
+        Status stat;
+        do {
+            stat = Status.GOOD;
+            System.out.println("Введите станцию назначения. (Максимальная длина названия - 83 символа. ");
+            input = scan.nextLine();
+            if (input.length() > 83) {
+                stat = Status.OUT_OF_RANGE;
+            }
+            System.out.println(ERRTEXT[stat.ordinal()]);
+        }while(stat != Status.GOOD);
+
+        return input;
+    }
+
+    public static int[] getDepartureTime(){
+
+        Status err;
+        String input = "";
+        int[] numbers = new int[2];
+        do {
+            err = Status.GOOD;
+            try {
+                input = scan.nextLine();
+                numbers[0] = Integer.parseInt(input.split(":")[0]);
+                numbers[1] = Integer.parseInt(input.split(":")[1]);
+            } catch (NumberFormatException e) {
+                err = Status.NOT_AN_INT;
+            }
+            if (err == Status.GOOD && (numbers[0] < MIN_HOUR || numbers[0] > MAX_HOUR || numbers[1] < MIN_MINUTE || numbers[1] > MAX_MINUTE
+                    || input.split(":")[0].length() != 2 || input.split(":")[1].length() != 2)) {
+                err = Status.OUT_OF_RANGE_TIME;
+            }
+            System.err.print(ERRTEXT[err.ordinal()]);
+        } while (err != Status.GOOD);
+
+
+
+
+        return numbers;
+    }
+
+    public static boolean getTickets(){
+        boolean isThereTickets = false;
+        Status stat = Status.GOOD;
+        String input;
+        do{
+            System.out.println("Введите наличие билетов (Есть/Нет).");
+            input = scan.nextLine().toLowerCase().trim();
+            if(input.equals("есть")){
+                isThereTickets = true;
+            }
+            else if(input.equals("нет")){
+                isThereTickets = false;
+            }
+            else {
+                stat = Status.OUT_OF_CHOICE;
+            }
+            System.err.printf(ERRTEXT[stat.ordinal()]);
+
+        }while(stat != Status.GOOD);
+
+        return isThereTickets;
+    }
+
+
+    public static void addTheTrain(List<Train> trains, String[] currentPath){
+
+        int trainNumber = getTrainNumber();
+        boolean trainFound = false;
+        String stationDest = "";
+        for(Train train : trains){
+            if(!trainFound && train.trainNumber == trainNumber){
+                trainFound = true;
+                stationDest = train.stationDest;
+
+            }
+        }
+
+        if (trainFound) {
+            System.out.println("Мы нашли поезд с этими данными (станция назначения автоматически заполнилась).");
+        }
+        else {
+            stationDest = getStation();
+        }
+        System.out.println("Введите время отправления в формате HH:MM.");
+        int[] departureTime = getDepartureTime();
+        boolean isThereTickets = getTickets();
+
+
+
+
+
+        Train trainNew = new Train(trainNumber, stationDest, departureTime[0], departureTime[1], isThereTickets);
+        trains.add(trainNew);
+        Status stat;
+        do {
+            stat = Train.saveTrains(trains, currentPath[0]);
+            if (stat != Status.GOOD) {
+                System.out.println(ERRTEXT[stat.ordinal()]);
+                createTrainsDat(trains, currentPath);
+            }
+        }while(stat != Status.GOOD);
+
     }
 
     public static void workWithDatabase(){
         boolean cantLeave = true;
         List<Train> trains = new ArrayList<Train>();
-        String currentPath = "";
-        openTrainsDat(trains, currentPath);
+        String[] currentPath = new String[1];
+        currentPath[0] = "";
+        createTrainsDat(trains, currentPath);
         while (cantLeave) {
             switch (getChoice()) {
                 case 0:
@@ -252,25 +480,50 @@ public class Main {
                     addTheTrain(trains, currentPath);
                     break;
                 case 2:
-                    changeTheTrain(trains, currentPath);
+                    if(trains.isEmpty()) {
+                        System.out.println("Список поездов пустой, сначала добавьте хоть один поезд.");
+                    }
+                    else {
+                        changeTheTrain(trains, currentPath);
+                    }
                     break;
                 case 3:
-                    deleteTrain(trains, currentPath);
+                    if(trains.isEmpty()) {
+                        System.out.println("Список поездов пустой, сначала добавьте хоть один поезд.");
+                    }
+                    else {
+                        deleteTrain(trains, currentPath);
+                    }
                     break;
                 case 4:
-                    showAllTrains(trains);
+                    if(trains.isEmpty()) {
+                        System.out.println("Список поездов пустой, сначала добавьте хоть один поезд.");
+                    }
+                    else {
+                        showAllTrains(trains);
+                    }
                     break;
                 case 5:
-                    showTrainsInTime(trains);
+                    if(trains.isEmpty()) {
+                        System.out.println("Список поездов пустой, сначала добавьте хоть один поезд.");
+                    }
+                    else {
+                        showTrainsInTime(trains);
+                    }
                     break;
                 case 6:
-                    checkTickets(trains);
+                    if(trains.isEmpty()) {
+                        System.out.println("Список поездов пустой, сначала добавьте хоть один поезд.");
+                    }
+                    else {
+                        checkTickets(trains);
+                    }
                     break;
                 case 7:
-                    currentPath = openTrainsDat(trains, currentPath);
+                    currentPath[0] = openTrainsDat(trains, currentPath);
                     break;
                 case 8:
-                    currentPath = createTrainsDat(trains, currentPath);
+                    currentPath[0] = createTrainsDat(trains, currentPath);
                     break;
             }
         }
@@ -281,8 +534,6 @@ public class Main {
     public static void main(String[] args) {
         writeTask();
         workWithDatabase();
-
-
         scan.close();
     }
 }
